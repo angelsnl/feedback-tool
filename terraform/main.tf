03941ef7f9546aaf74d438daa4ad44dff27fe940
Joona Kärkkäinen <joona.karkkainen@netlight.com>
2026-05-26 10:13:44 +0300

    feat: new terraform setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,84 +5,16 @@ terraform {
       version = "~> 5.0"
     }
   }
+  required_version = ">= 1.3"
+  cloud {
+    organization = "pepes-angels-test"
+    workspaces {
+      name = "upcloud-test"
+    }
+  }
+
 }
 
 provider "upcloud" {
-  username = var.upcloud_username
-  password = var.upcloud_password
-}
-
-data "upcloud_storage" "ubuntu" {
-  most_recent = true
-  type        = "template"
-  name_regex  = "Ubuntu Server 22\\.04"
-}
-
-resource "upcloud_managed_database_postgresql" "db" {
-  name  = "${var.project_name}-db"
-  plan  = "1x1xCPU-2GB-25GB"
-  zone  = var.zone
-  title = "${var.project_name} PostgreSQL"
-
-  properties {
-    public_access = true
-    ip_filter     = var.db_allowed_ips
-  }
-}
-
-resource "upcloud_server" "app" {
-  hostname = "${var.project_name}-app"
-  plan     = "1xCPU-1GB"
-  zone     = var.zone
-
-  template {
-    storage = data.upcloud_storage.ubuntu.id
-    size    = 25
-  }
-
-  network_interface {
-    type = "public"
-  }
-
-  network_interface {
-    type = "utility"
-  }
-
-  login {
-    user            = "root"
-    keys            = [var.ssh_public_key]
-    create_password = false
-  }
-}
-
-resource "upcloud_firewall_rules" "app" {
-  server_id = upcloud_server.app.id
-
-  firewall_rule {
-    action                 = "accept"
-    direction              = "in"
-    family                 = "IPv4"
-    protocol               = "tcp"
-    destination_port_start = "3000"
-    destination_port_end   = "3000"
-    comment                = "Node app"
-  }
-
-  firewall_rule {
-    action                 = "accept"
-    direction              = "in"
-    family                 = "IPv4"
-    protocol               = "tcp"
-    destination_port_start = "22"
-    destination_port_end   = "22"
-    source_address_start   = var.admin_ip
-    source_address_end     = var.admin_ip
-    comment                = "SSH from admin"
-  }
-
-  firewall_rule {
-    action    = "drop"
-    direction = "in"
-    comment   = "Drop all other inbound"
-  }
+  token = var.upcloud_token
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,20 +1,36 @@
-output "server_ip" {
-  description = "App server public IP"
+output "server_public_ip" {
+  description = "Public IP address of the app server"
   value       = upcloud_server.app.network_interface[0].ip_address
 }
 
-output "db_service_uri" {
-  description = "PostgreSQL connection string (use as DATABASE_URL)"
-  value       = upcloud_managed_database_postgresql.db.service_uri
-  sensitive   = true
-}
-
 output "db_host" {
-  description = "PostgreSQL host"
-  value       = upcloud_managed_database_postgresql.db.service_host
+  description = "PostgreSQL service hostname"
+  value       = upcloud_managed_database_postgresql.main.service_host
 }
 
 output "db_port" {
-  description = "PostgreSQL port"
-  value       = upcloud_managed_database_postgresql.db.service_port
+  description = "PostgreSQL service port"
+  value       = upcloud_managed_database_postgresql.main.service_port
+}
+
+output "db_name" {
+  description = "Default database name"
+  value       = upcloud_managed_database_postgresql.main.primary_database
+}
+
+output "db_username" {
+  description = "PostgreSQL admin username"
+  value       = upcloud_managed_database_postgresql.main.service_username
+}
+
+output "db_password" {
+  description = "PostgreSQL admin password"
+  value       = upcloud_managed_database_postgresql.main.service_password
+  sensitive   = true
+}
+
+output "db_uri" {
+  description = "Full PostgreSQL connection URI"
+  value       = upcloud_managed_database_postgresql.main.service_uri
+  sensitive   = true
 }
