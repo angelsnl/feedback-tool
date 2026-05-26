22959856a3d508e0cf4fed8ff0cd758b334ac22a
Victor Keil <victor.keil@netlight.com>
2026-05-26 10:15:31 +0300

    chore: remove unnecessary tf local dir

--- terraform/local/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "database_url" {
-  description = "DATABASE_URL connection string for the app"
-  value       = "postgresql://${var.app_db_user}:${var.app_db_password}@${var.pg_host}:${var.pg_port}/${var.app_db_name}?sslmode=disable"
-  sensitive   = true
-}
